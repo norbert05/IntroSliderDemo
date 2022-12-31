102f1bba198a3a4de5d4b740e21fdbf9a9220319
Norbert Horvath <norbi0508@gmail.com>
2022-12-31 08:56:27 +0100

    Initial Commit

---
+++ IntroSlider/IntroSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntroSliderApp.swift
+//  IntroSlider
+//
+//  Created by Norbert Horváth on 2022. 12. 31..
+//
+
+import SwiftUI
+
+@main
+struct IntroSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
